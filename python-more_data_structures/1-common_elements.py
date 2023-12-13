d8d4c2766f184b0b452affaeb70d6e3e4d1992ef
AAkindimeji <141722088+AAkindimeji@users.noreply.github.com>
2023-12-13 13:47:57 +0100

    Checking

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -3,7 +3,7 @@ def common_elements(set_1, set_2):
     return set_1.intersection(set_2)
 
 # Test case
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-c_set = common_elements(set_1, set_2)
-print(sorted(list(c_set)))
+# set_1 = { "Python", "C", "Javascript" }
+# set_2 = { "Bash", "C", "Ruby", "Perl" }
+# c_set = common_elements(set_1, set_2)
+# print(sorted(list(c_set)))
