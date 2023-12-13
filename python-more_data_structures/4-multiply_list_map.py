9c14ee1274dfcc1750b2e420e2a75d5bfc836a7c
AAkindimeji <141722088+AAkindimeji@users.noreply.github.com>
2023-12-13 13:57:49 +0100

    Check

--- python-more_data_structures/4-multiply_list_map.py
+++ python-more_data_structures/4-multiply_list_map.py
@@ -1,7 +1,7 @@
 def multiply_list_map(my_list=[], number=0):
     return list(map(lambda x: x * number, my_list))
 
-# # Test case
+# Test case
 # my_list = [1, 2, 3, 4, 6]
 # new_list = multiply_list_map(my_list, 4)
 # print(new_list)
